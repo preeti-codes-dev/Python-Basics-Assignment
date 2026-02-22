0ce3d8e564acf942fbc03a22a904c76957b01625
Preeti Patil <119400681+preetipatil-dot@users.noreply.github.com>
2026-02-22 12:07:46 +0530

    Completed Q2 - Simple Calculator

---
+++ q2.py
@@ -0,0 +1,35 @@
+# Q2: Simple Calculator
+
+# Taking input from user
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    print("Results:")
+
+    # Addition
+    print(num1, "+", num2, "=", num1 + num2)
+
+    # Subtraction
+    print(num1, "-", num2, "=", num1 - num2)
+
+    # Multiplication
+    print(num1, "*", num2, "=", num1 * num2)
+
+    # Division
+    if num2 != 0:
+        print(num1, "/", num2, "=", round(num1 / num2, 2))
+    else:
+        print("Division by zero is not allowed")
+
+    # Modulus
+    if num2 != 0:
+        print(num1, "%", num2, "=", num1 % num2)
+    else:
+        print("Modulus by zero is not allowed")
+
+    # Exponentiation
+    print(num1, "^", num2, "=", num1 ** num2)
+
+except ValueError:
+    print("Invalid input! Please enter numbers only.")
